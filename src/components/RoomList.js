d78246c3cfb713ed321fdece4e99e24b954500aa
Lukegits <luket.chavez@gmail.com>
2018-04-01 08:40:53 -0600

    chat rooms work 41 840pm

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,15 +5,31 @@ class RoomList extends Component {
  constructor(props) {
    super(props);
 
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.roomsRef = this.props.firebase.database().ref('rooms');
 
-    this.state = {
+      this.state = {
         rooms: []
       };
    }
+   createRoom() {
+     this.roomsRef.push({
+       rooms: this.createRoom.newName
+     });
+   }
+   componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+        const room = snapshot.val();
+        room.key = snapshot.key;
+        this.setState({ rooms: this.state.rooms.concat( room ) })
+       });
+     }
 
    render(){
      return (
+       <form className="createroom">
+         New Chat Room Name:
+         <input type="text" defaultValue='Example' onChange={this.createRoom.newName} />
+         <input type="submit" onSubmit={this.createRoom} />
        <table className='applistsidebar'>
          <tbody className='listofroomstop'>
           <tr>
@@ -23,15 +39,8 @@ class RoomList extends Component {
           </tr>
          </tbody>
        </table>
-     )
-   }
-
-  componentDidMount() {
-   this.roomsRef.on('child_added', snapshot => {
-       const room = snapshot.val();
-       room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( room ) })
-      });
+     </form>
+      )
     }
   }
 
